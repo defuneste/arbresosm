d09c32ffce61af7f08923b4be4768e57b1055245
defuneste <leroy.oli@gmail.com>
2018-11-30 16:12:43 +0100

    profil user arbre

--- exploraton_nettoyage.R
+++ exploraton_nettoyage.R
@@ -40,7 +40,7 @@ drv <- dbDriver("PostgreSQL")
 # fais un pont vers la db réutilisable
 # ici j'ai pris une db en local pour tester
 # con sera utilisé pour chaque connection et pkoi le franciser
-con <- dbConnect(drv, dbname = "osmdbfranceu",
+con <- dbConnect(drv, dbname = "osmgouv",
                  host = "localhost", port = 5432, # attention 5432 par défaut
                  user = "postgres", password = pw) # idem pour user
 rm(pw) # mouais
@@ -172,8 +172,6 @@ filter(name == "Formica III") # une version avec NSE
 
 france.shp$surface_ha <-  set_units(st_area(france.shp), value = ha) # on calcul la surface en ha
 
-france.shp$name
-
 # on filtre par la surface pour enlever les paradis fiscaux
 # on a encore un problème avc des îles italiennes aux larges de la corse, on vire Toscanna dans les données geofabrik
 # il y a un bout de la suisse dans les données data gouv
@@ -249,7 +247,7 @@ species.dat %>%
 ######## travail sur osm_user et sur l'encodage des arbres
 
 # import des conributeurs et du moment de la contribution
-user.dat <- dbGetQuery(con,  "SELECT  tags -> 'osm_timestamp' AS ts, tags -> 'osm_user' AS user
+user.dat <- dbGetQuery(con,  "SELECT  tags -> 'osm_timestamp' AS ts, tags -> 'osm_user' AS nom
 FROM planet_osm_point
 WHERE planet_osm_point.natural = 'tree';")
 
@@ -265,35 +263,74 @@ ggplot(user.dat, aes(x = ts)) +
     xlab("Années") +
     ylab("Nb. arbres isolés")
 
-user.dat %>% 
+# liste des utilisteurs avec le nombre d'arbres
+profil_user.dat <- user.dat %>% 
     #filter(ts > "2018-11-01") %>% 
-    group_by(user) %>% 
-    summarise(nb = n()) %>% 
-    arrange(desc(nb))
+    group_by(nom) %>% 
+    summarise(nb_arbre = n(),
+              duree = max(ts) - min(ts)) %>% 
+    arrange(desc(nb_arbre))
 
-    print(n = Inf)
+## comptage du nombre de jour ou un arbre à été ajouté modifié
 
+nb_jour <- user.dat %>% 
+    #filter(ts > "2018-11-01") %>% 
+    group_by(nom, ts) %>% 
+    summarise(tot = 1) %>%
+    group_by(nom) %>% 
+    summarise(nb_jour =  sum(tot))
+
+# une petite verification avec une user
 
 user.dat %>% 
-    group_by(user) %>% 
-    summarise(nb = n()) %>% 
-ggplot(aes(x = nb)) +
-    geom_histogram(binwidth = 50) 
+    filter(nom == "_phiphou_") %>%
+    distinct(ts)
 
-+ 
-    scale_x_log10()
+# join attention sur le meme nom
+profil_user.dat <- profil_user.dat %>%
+    left_join(nb_jour, by = "nom")
 
-%>%  
+dim(profil_user.dat)
+
+## constitution d'un tableau pour les profils d'utilisteurs : 
+
+poly_count.dat <- dbGetQuery(con, "SELECT tags -> 'osm_user' AS nom, COUNT (*) AS poly_count
+                            FROM planet_osm_polygon
+                            WHERE  tags -> 'osm_user' IN (SELECT DISTINCT tags -> 'osm_user' AS nom
+                                                        FROM planet_osm_point
+                                                        WHERE planet_osm_point.natural = 'tree')
+                            GROUP BY nom
+                            ORDER BY poly_count DESC;")
+dim(poly_count.dat)
     
-    group_by(nb) %>% 
-    summarise(n()) %>%
-    print(n = Inf)
-    
-    
 
+ligne_count.dat <- dbGetQuery(con, "SELECT tags -> 'osm_user' AS nom, COUNT (*) AS ligne_count
+                              FROM planet_osm_roads
+                              WHERE  tags -> 'osm_user' IN (SELECT DISTINCT tags -> 'osm_user' AS nom
+                                                            FROM planet_osm_point
+                                                            WHERE planet_osm_point.natural = 'tree')
+                              GROUP BY nom
+                              ORDER BY ligne_count DESC;" )
+dim(ligne_count.dat)
+
+
+poi_count.dat <- dbGetQuery(con, "SELECT tags -> 'osm_user' AS nom, COUNT (*) AS poi_count
+                            FROM planet_osm_point
+                            WHERE  tags -> 'osm_user' IN (SELECT DISTINCT tags -> 'osm_user' AS nom
+                                                          FROM planet_osm_point
+                                                          WHERE planet_osm_point.natural = 'tree')
+                            GROUP BY nom
+                            ORDER BY poi_count DESC;")
+dim(poi_count.dat)
+
+# join attention meme nom 
+
+profil_user.dat <- profil_user.dat %>%
+    left_join(poi_count.dat, by = "nom") %>%
+    left_join(ligne_count.dat, by = "nom") %>% 
+    left_join(poly_count.dat , by = "nom") 
     
-
-
+write.csv(profil_user.dat, "profile.csv")
 
 # des greps pour tester des pseudo de com com
 # unique(user.dat$user)[grep(pattern = "Paris|paris", unique(user.dat$user))]
