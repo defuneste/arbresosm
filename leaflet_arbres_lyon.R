d3ee29d33a69d95273a40fb941db1ea33b71fe35
defuneste <leroy.oli@gmail.com>
2019-02-06 16:04:11 +0100

    ajout d'une legende

--- leaflet_arbres_lyon.R
+++ leaflet_arbres_lyon.R
@@ -12,24 +12,33 @@ library(leaflet)
 
 unique(arbres_lyon_final.shp$codeinsee)
 
-quartier <- arbres_lyon_final.shp %>%
-    filter(codeinsee == "69003")
+quartier <-  arbres_lyon_final.shp %>%
+    filter(codeinsee == "69063")
 class(quartier)
-
-carto_lyon <- leaflet() %>%
-    addTiles() %>%
-    addCircleMarkers(data = quartier, radius = 2, 
-                     color = ~pal(localisati))
-
-carto_lyon
 
 ## les categories de denotation
 
 summary(arbres_lyon_final.shp$localisati)
 
-pal <- colorFactor(palette =c("green", "red", "brown", "grey", "pink"),
-                   levels = c("Espace vert", "Lieu de stationnement ", "Terre plein latéral",
-                              "TPC ou rond point", "Trottoir ou domaine piétonnier"))
+pal <- colorFactor(palette =c("green", "red", "brown", "blue", "yellow"),
+                   levels = c("Espace vert", "Lieu de stationnement", "Terre plein latéral",
+                              "TPC ou rond point", "Trottoir ou domaine piétonnier"),
+                   na.color = "black")
+
+
+carto_lyon <- leaflet() %>%
+    addTiles() %>%
+    addCircleMarkers(data = quartier, radius = 2, 
+                     color = ~pal(localisati)) %>%
+    addLegend(position = "bottomright",
+              pal = pal,
+              values = c("Espace vert", "Lieu de stationnement", "Terre plein latéral",
+                         "TPC ou rond point", "Trottoir ou domaine piétonnier"),
+              na.label = "abs d'info")
+
+carto_lyon
+
+
 
 
 
