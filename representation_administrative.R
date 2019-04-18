ec9e1963d1a9e649b7e85235eee8a8c6845a4996
defuneste <leroy.oli@gmail.com>
2019-04-18 22:28:55 +0200

    jointure pour type attemyion des na

--- representation_administrative.R
+++ representation_administrative.R
@@ -32,16 +32,19 @@ library(units) # gestion des unités pour ha
 library(rmapshaper) #Visvalingam’s algorithm pour ms_simplify
 
 
-
 #### lecture des communes en france source geofla 
 
 commune.shp <- st_read("data/GEOFLA_COMMUNE_2016/COMMUNE_WGS84.shp")
 summary(commune.shp)
+str(commune.shp)
+
 type_commune.dat <- read.csv("data/type_commune.csv", sep = "\t")
 summary(type_commune.dat)
+str(type_commune.dat)
 
-#commune_type.shp <- commune.shp %>% 
+commune_type.shp <- commune.shp %>% 
+    left_join(type_commune.dat, by = c("INSEE_COM" = "CODGEO"))
     
+table(commune_type.shp$STATUT_2016)
 
-class(commune.shp)
-summary(commune.shp)
+sum(table(commune_type.shp$STATUT_2016))
