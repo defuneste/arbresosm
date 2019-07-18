6fbd4bba15db7d7793f981828cc57d5e0a83ca11
defuneste <leroy.oli@gmail.com>
2019-07-18 16:51:37 +0200

    présentation + courbe de lorentz

--- user_info.R
+++ user_info.R
@@ -5,7 +5,11 @@
 # pour me rapeller qu'un objet à des infos geometriques type vecteur je lui accolle un .shp
 # si c'est un raster .grid, si c'est un dataframe souvent un .dat
 
-# chargement des différents packages demandés 
+##.###################################################################################33
+## I Chargement des différents packages demandés ====
+##.#################################################################################33
+
+#  
 
 ### DB
 library(RPostgreSQL) # fait le lien avec postgre, utilise DBI
@@ -50,10 +54,15 @@ con <- dbConnect(drv, dbname = "franceuser",
                  user = "postgres", password = pw) # idem pour user
 rm(pw) # mouais
 
-######## travail sur osm_user et sur l'encodage des arbres
+
+##.###################################################################################33
+## II Ouverture et preprocessing des données ====
+##.#################################################################################33
+
+##    1 - Recup des Arbres OSM  ================
 
 # import des conributeurs et du moment de la contribution
-user.shp <- dbGetQuery(con,  "SELECT  tags -> 'osm_timestamp' AS ts, tags -> 'osm_user' AS nom, tags -> 'osm_changeset' AS changeset,
+user.shp <- st_read(con,  query = "SELECT  way, tags -> 'osm_timestamp' AS ts, tags -> 'osm_user' AS nom, tags -> 'osm_changeset' AS changeset,
                        osm_id AS ID, tags -> 'osm_uid' AS user_id, tags -> 'osm_version' AS version
                        FROM planet_osm_point
                        WHERE planet_osm_point.natural = 'tree';")
@@ -61,41 +70,114 @@ user.shp <- dbGetQuery(con,  "SELECT  tags -> 'osm_timestamp' AS ts, tags -> 'os
 dim(user.shp)
 str(user.shp)
 
-# # on prend les communes dans OSM c'est plus à jour que geofla
-# commune_osm.shp  <- st_read(con,  query = "SELECT name,tags -> 'ref:INSEE' AS INSEE, way  
-#                                             FROM planet_osm_polygon
-#                                             WHERE boundary = 'administrative'  AND admin_level = '8';")
-# summary(commune_osm.shp) # petits verifs
-# st_crs(commune_osm.shp) # verification du CRS
-# 
-# # on utilise mapshaper il faut js mapshaper d'installer surtout ici avec sys= T 
-# commune_simplify.shp <- ms_simplify(commune_osm.shp, sys = TRUE) 
-# 
-# commune_type.shp <- commune_simplify.shp %>% 
-#     filter(!is.na(insee))
-# 
-# france.shp <- commune_type.shp %>%
-#     st_union() # un gros merge sur l'ensemble
-# st_crs(france.shp) # verif du CRS
-# 
-# user_france.shp <- species.shp[france.shp,] # je coupe avec la france
-# 
-# user.dat <- user_france.shp %>%
-#     st_set_geometry(value = NULL) %>% #drop de la geometrie
-# 
-# user.dat$ts <- as_date(user.dat$ts) # on passe en POSIX juste date
-# head(user.dat)
+##    2 - Recup des shape util  ================
+# on prend les communes dans OSM c'est plus à jour que geofla
+commune_osm.shp  <- st_read(con,  query = "SELECT name,tags -> 'ref:INSEE' AS INSEE, way  
+                                            FROM planet_osm_polygon
+                                            WHERE boundary = 'administrative'  AND admin_level = '8';")
+summary(commune_osm.shp) # petits verifs
+st_crs(commune_osm.shp) # verification du CRS
+
+# on prend les regions dans OSM pour un fond
+france.shp <- st_read(con,  query = "SELECT name, way
+                                    FROM planet_osm_polygon
+                                    WHERE boundary = 'administrative'  AND admin_level = '4';")
+
+# un simplify, il faut js et la library mapshaper d'installer
+# sys = TRUE l'utilise et evite de passer par une API
+france_simplify.shp <- ms_simplify(france.shp, sys = TRUE) 
+# plot(france_simplify.shp) verif
+
+france.shp <- commune_type.shp %>%
+    filter(!is.na(insee)) %>% 
+    st_union() # un gros merge sur l'ensemble
+
+st_crs(france.shp) # verif du CRS
+
+##    3 - Preprocessing des données  ================
+
+user_france.shp <- user.shp[france.shp,] # je coupe avec la france
+
+user.dat <- user_france.shp %>%
+    st_set_geometry(value = NULL)  #drop de la geometrie
+
+user.dat$ts <- as_date(user.dat$ts) # on passe en POSIX juste date
+head(user.dat) # une verif
+
+##.###################################################################################33
+## III Travail sur osm_user et sur l'encodage des arbress ====
+##.#################################################################################33
+
+
+##    1 - Evolution dans le temps des arbres dans OSM   ================
 
 # j'ai pris la 15aine mais on est presque de l'ordre du jour
+# ici sur un fichier léger
+
 ggplot(user.dat, aes(x = ts)) +
     geom_histogram(binwidth = 15) +
     xlab("Années") +
     ylab("Nb. arbres isolés") +
     labs(caption ="source : © les contributeurs d’OpenStreetMap")
 
+# si on veut aller un peu plus loin et verifier d'ou vient la donnée on peut utilsier les champs refs
+# il faut prendre cette base dans exploration, je devrais pe en faire une version r binary pour gagner 
+# du temps
+
+# un grep pour avoir les noms de variables qui commence par ref
+import_bd <- names(arbres_osm)[grep(pattern = "^ref", names(arbres_osm))]
+
+# ref est etrange et doit être un peu osculté, il semble cependant bien que ce soit une ref d'une base
+
+pas_na <- function(x) {sum(!is.na(x))} # une fonction qui somme les T/F sur des pas Na
+
+# ici je compte les nom Na pour les refs
+lapply(arbres_osm[names(arbres_osm)[grep(pattern = "^ref", names(arbres_osm))]], pas_na)
+
+arbres_osm$refbd <- apply(arbres_osm[,import_bd], 1, pas_na)
+
+table(arbres_osm$refbd)  # j'ai des cheffauchement au max 3 et souvent 2 sur Paris
+
+# ici je prefére du 0/1 mais c'est discutable
+arbres_osm$refbd[arbres_osm$refbd >= 1] <- 1
+
+# une petite verif visuelle : il y a pb des erreurs avec "ref"
+# arbres_osm %>% 
+#     filter(refbd == 1) %>% 
+#     select(import_bd, refbd) %>% 
+#     view()
+
+# on passe dans un format dates
+arbres_osm$osm_timestamp <- as.Date(arbres_osm$osm_timestamp) # en date attention on perd l'heure/minutes
+
+ggplot(arbres_osm, aes(x = osm_timestamp, fill = as.factor(refbd))) +
+    geom_histogram(binwidth = 15) +
+    xlab("Year") +
+    ylab("Isolated trees") +
+    labs(caption ="© OpenStreetMap contributors") + 
+    guides(fill=guide_legend(title="Ref. from DB import:")) +
+    scale_fill_manual(labels = c("- Not indicated", "- Indicated"), values = c("#1b9e77", "#7570b3"))
+
+
+## ici une version de ce graph en tenant compte de l'info botanique
+arbres_osm$info <- 0 # un nouveau champ
+arbres_osm$info[!is.na(arbres_osm$genus)] <- 1 # il prend 1 quamd j'ai une info sur genus
+arbres_osm$info[!is.na(arbres_osm$species)] <- 1 # il prend 1 quamd j'ai une info sur species
+
+
+ggplot(arbres_osm, aes(x = osm_timestamp, fill = as.factor(info))) +
+    geom_histogram(binwidth = 15) +
+    xlab("Year") +
+    ylab("Isolated trees") +
+    labs(caption ="© OpenStreetMap contributors") +
+    guides(fill=guide_legend(title="Botanic information:")) +
+    scale_fill_manual(labels = c("- not indicated", "- indicated"), values = c("#d95f02", "#7570b3"))
+
 # une verif pour s'assurer que osm_uid correspond bien à l'id de l'user
 user.dat %>% 
     filter(nom == "defuneste")
+
+##    2 - Contribution cumulée de chaques utilisateurs    ================
 
 # liste des utilisteurs avec le nombre d'arbres
 liste_user.dat <- user.dat %>% 
@@ -103,10 +185,30 @@ liste_user.dat <- user.dat %>%
     group_by(nom) %>% 
     summarise(nb_arbre = n(),
               duree = max(ts) - min(ts)) %>% 
-    arrange(desc(nb_arbre)) 
+    arrange(nb_arbre) %>% # ici je classe par par ordre descendant car je vais utiliser plus loin pour faire une courbe de lorentz
+    mutate(order = 1:length(liste_user.dat$nom)) 
 
 #  nombre d'utilsateurs contribuant aux arbres
 dim(liste_user.dat)
+
+median(liste_user.dat$nb_arbre) # mediane
+mean(liste_user.dat$nb_arbre) # moyenne
+
+liste_user.dat$cumsum_nbarbre <- cumsum(liste_user.dat$nb_arbre) # somme cumulée des arbres
+
+# ici order est divisé par son max et pareil pour la somme cumuléé
+# cela les passe de 0 à 1 et on modifie les labels avec 
+# scales::percent
+ggplot(liste_user.dat, aes(x = order/max(liste_user.dat$order), y = cumsum_nbarbre/max(liste_user.dat$cumsum_nbarbre))) +
+    geom_line(col = "forestgreen", lwd = 1.5) +
+    scale_x_continuous(labels = scales::percent) +
+    scale_y_continuous(labels = scales::percent) +
+    labs( x = "Pourcentage d'arbres ajoutés",
+          y = "Pourcentage de contributeurs", 
+          caption = "© OpenStreetMap contributors") + 
+    theme_bw()
+
+plot(liste_user.dat$cumsum_nbarbre, liste_user.dat$nb_arbre)
 
 ## comptage du nombre de jour ou un arbre à été ajouté modifié
 
